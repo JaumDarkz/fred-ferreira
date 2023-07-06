0562083f325c74ecc7fbe3b4c992ae5131e07061
João Vinícius <viniciuspkfr@gmail.com>
2023-07-05 21:40:53 -0300

    fix: change wpp number

--- components/CTA/index.tsx
+++ components/CTA/index.tsx
@@ -7,7 +7,7 @@ const CTA = () => {
     'https://wa.me/5531993144678?text=Ola%2C+vi+o+video+no+tiktok+e+quero+entender+melhor',
     'https://wa.me/5531993144853?text=Ola%2C+vi+o+video+no+tiktok+e+quero+entender+melhor',
     'https://wa.me/5531993145010?text=Ola%2C+vi+o+video+no+tiktok+e+quero+entender+melhor',
-    'https://wa.me/5531993145063?text=Ola%2C+vi+o+video+no+tiktok+e+quero+entender+melhor'
+    'https://wa.me/5531993145153?text=Ola%2C+vi+o+video+no+tiktok+e+quero+entender+melhor'
   ]
 
   const openRandomLink = () => {
